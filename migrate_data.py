fa08747457ce1922d618f0bdb8cf7ed108d9c535
ultimocorreoexistente-prog <ultimo.correo.existente@gmail.com>
2025-11-22 17:01:44 -0300

    FIX: Corregida importación y uso de EquipoTecnico en migrate_data.py

--- migrate_data.py
+++ migrate_data.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from app import app, db
 from models import (Usuario, Ubicacion, Camara, Gabinete, Switch, PuertoSwitch,
                    UPS, NVR, FuentePoder, CatalogoTipoFalla, Falla, 
-                   Mantenimiento, Equipo_Tecnico)
+                   Mantenimiento, EquipoTecnico)
 from werkzeug.security import generate_password_hash
 import os
 import re
